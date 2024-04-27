2413a9de53d210827900a1791b8044d92326b217
hazinofan <ahmedaminemaarouf1@gmail.com>
2024-04-27 21:21:13 +0100

    updating API route

--- api/routes/user.Routes.js
+++ api/routes/user.Routes.js
@@ -1,10 +1,12 @@
 import express from 'express'
-import { test } from '../../controllers/user.Controller.js';
+import { test, updateUser } from '../../controllers/user.Controller.js';
+import { verifyToken } from '../utils/VerifiyUser.js';
 
 
 
 const router = express.Router();
-
+ 
 router.get('/test' , test)
+router.put('/update/:userId' ,verifyToken, updateUser)
 
 export default router ;
--- controllers/user.Controller.js
+++ controllers/user.Controller.js
@@ -1,3 +1,50 @@
+import User from "../api/models/user.model.js"
+import { errorHandler } from "../api/utils/error.js"
+import bcryptjs from 'bcryptjs'
+
 export const test = (req,res) => {
     res.json({ message : 'API is working! '})
+}
+
+
+export const updateUser = async (req, res, next ) => {
+    if(req.user.id !== req.params.userId) {
+        return next(errorHandler(403, 'You are not allowed  to update this User'))
+    }
+    if(req.body.password){
+    if(req.body.password.length < 6) {
+        return next(errorHandler(   400, 'Password must be at least 6 Characters Long'))
+    }
+    req.body.password = bcryptjs.hashSync(req.body.password, 10);
+}
+    if(req.body.username){
+    if(req.body.username.length < 7 || req.body.username.length > 20 ) {
+        return next(errorHandler(400, 'Username must be between 7 and 20 characters'))
+    }
+    if(req.body.username.includes(' ')){
+        return next(errorHandler(400, 'Username Cannot contain spaces'))
+        }
+    if(req.body.username !== req.body.username.toLowerCase()){
+            return next(errorHandler(400, 'Username must be lowercase'))
+        }
+    if(!req.body.username.match(/^[a-zA-Z0-9]+$/)){
+            return next(errorHandler(400, 'Username Can only contain numbers and letters'))
+        }
+        try {
+            const updateUser = await User.findByIdAndUpdate(req.params.userId, {
+                $set: {
+                    username: req.body.username,
+                    email: req.body.email,
+                    ProfilePicture: req.body.ProfilePicture,
+                    password: req.body.password
+                }
+            }, { new: true });
+            const {password,...rest} = updateUser._doc;
+            res.json(200).json(rest)
+        } catch (error) {
+            next(error);
+        }
+        
+        
+    }
 }
