3f315e50fe67e63823a4306d9bf46fd76d4e850f
hazinofan <ahmedaminemaarouf1@gmail.com>
2024-05-04 19:55:52 +0100

    add delete user functionnality to the admin

--- client/src/components/DashUsers.jsx
+++ client/src/components/DashUsers.jsx
@@ -52,6 +52,23 @@ function DashUsers() {
     }
   }
 
+  const handleDeleteUser = async () => {
+    try {
+        const res = await fetch(`/api/user/delete/${userIdToDelete}`, {
+            method: 'DELETE',
+        });
+        const data = await res.json();
+        if (res.ok) {
+            setUsers((prev) => prev.filter((user) => user._id !== userIdToDelete));
+            setShowModal(false);
+        } else {
+            console.log(data.message);
+        }
+    } catch (error) {
+        console.log(error.message);
+    }
+  };
+
   return (
     <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
       {currentUser.isAdmin && users.length > 0 ? (
@@ -100,18 +117,18 @@ function DashUsers() {
         <p>You have no Users Yet !</p>
       )}
       <Modal show={showModal} onClose={()=>setShowModal(false)} popup size='md'>
-        <Modal.Header />
-        <Modal.Body>
-          <div className="text-center">
-            <HiOutlineExclamationCircle className='h-14 w-14 text-gray-400 dark:tex-gray-200 mb-4 mx-auto' />
-            <h3 className="mb-5 text-lg text-gray-500 dark:text-gray-400"> Are you sure You want to delete This User ? </h3>
-            <div className="flex justify-center gap-4">
-              <Button color='failure'>Yes i'm sure </Button>
-              <Button color='gray' onClick={()=> setShowModal(false)}>Yes i'm sure </Button>
-            </div>
-          </div>
-        </Modal.Body>   
-      </Modal>
+            <Modal.Header />
+              <Modal.Body>
+                <div className="text-center">
+                <HiOutlineExclamationCircle className='h-14 w-14 text-gray-400 dark:tex-gray-200 mb-4 mx-auto' />
+                <h3 className="mb-5 text-lg text-gray-500 dark:text-gray-400"> Are you sure You want to delete This Post ? </h3>
+                <div className="flex justify-center gap-4">
+                  <Button color='failure' onClick={handleDeleteUser}>Yes i'm sure </Button>
+                  <Button color='gray' onClick={()=> setShowModal(false)}>No , Cancel </Button>
+                </div>
+                </div>
+              </Modal.Body>   
+         </Modal>
     </div>
   );
 }
